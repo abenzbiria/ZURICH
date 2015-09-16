83968384c2078bee5d7a475a820620911aaf10aa
msalim <msalim@kazacube.com>
2015-09-16 12:59:39 +0100

    add onchange rubrique_id

--- zurich_purchase/purchase.py
+++ zurich_purchase/purchase.py
@@ -15,23 +15,25 @@ class purchase_order(osv.osv):
     _inherit = "purchase.order"
 
 
-    state = fields.Many2one(string="Statut", comodel_name="purchase.order.stage",)
+    state = fields.Many2one(string="Statut", comodel_name="purchase.order.stage",domain="[('rubrique_ids','=',rubrique_id)]")
     rubrique_id = fields.Many2one(comodel_name="rubrique.rubrique", string="Rubrique",default=1)
 
-    @api.onchange('rubrique_id')
-    def get_selection(self):
-        if not self.rubrique_id:
-            return
-        rubrique = self.rubrique_id
-        ids = []
-        for stage in rubrique.stage_ids:
-            ids.append(stage.id)
+    # @api.onchange('rubrique_id')
+    # def get_selection(self):
+    #     rubrique = self.rubrique_id
+    #     ids = [x.id for x in rubrique.stage_ids]
+    #     domain=[('id','in',tuple(ids))]
+    #     self.rubrique_id = rubrique.id
+    #     return {'domain':{'state':domain}}
+
+    def onchange_rubrique_id(self,cr,uid,ids,rubrique_id,context=None):
+        if not rubrique_id:
+            return {}
+        rubrique = self.pool.get('rubrique.rubrique').browse(cr,uid,rubrique_id)
+        ids = [x.id for x in rubrique.stage_ids]
         domain=[('id','in',tuple(ids))]
-        print '1111111111',domain
+        print '2222222',rubrique.name,domain
         return {'domain':{'state':domain}}
-
-
-
 
 purchase_order()
 
